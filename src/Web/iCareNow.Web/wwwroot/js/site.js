058ba2473081687935bba642cdbe21e0f1ae0395
Lounderr <77444667+Lounderr@users.noreply.github.com>
2022-11-28 00:08:53 +0200

    Add login partial to nav and optimize site.js

--- src/Web/iCareNow.Web/wwwroot/js/site.js
+++ src/Web/iCareNow.Web/wwwroot/js/site.js
@@ -1,29 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-
-
-
-function toggleMenu() {
-    const navContainer = document.getElementsByClassName('nav-items-container')[0];
-    const controls = document.getElementsByClassName('menu-controls')[0];
-
-    controls.classList.toggle('menu-active');
-    navContainer.classList.toggle('nav-active');
-
-    if (this.navContainer.classList.contains('nav-active')) {
-        document.body.style.overflow = 'hidden';
-    } else {
-        document.body.style.overflow = 'visible';
-    }
-}
+﻿const navContainer = document.querySelector('.nav-items-container');
+const controls = document.querySelector('.menu-controls');
 
 addEventListener("resize", (event) => {
-    let navContainer = document.getElementsByClassName('nav-items-container')[0];
-    let controls = document.getElementsByClassName('menu-controls')[0];
-
     controls.classList.remove('menu-active');
     navContainer.classList.remove('nav-active');
 });
+
+function toggleMenu() {
+    navContainer.classList.toggle('nav-active');
+    controls.classList.toggle('menu-active');
+}
