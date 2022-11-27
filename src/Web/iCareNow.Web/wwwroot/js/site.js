a9a21353e25a95084a7cad9b762f031e053e0adc
Lounderr <77444667+Lounderr@users.noreply.github.com>
2022-11-27 21:45:59 +0200

    Create navigation bar and first section of page

--- src/Web/iCareNow.Web/wwwroot/js/site.js
+++ src/Web/iCareNow.Web/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+
+function toggleMenu() {
+    const navContainer = document.getElementsByClassName('nav-items-container')[0];
+    const controls = document.getElementsByClassName('menu-controls')[0];
+
+    controls.classList.toggle('menu-active');
+    navContainer.classList.toggle('nav-active');
+
+    if (this.navContainer.classList.contains('nav-active')) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = 'visible';
+    }
+}
+
+addEventListener("resize", (event) => {
+    let navContainer = document.getElementsByClassName('nav-items-container')[0];
+    let controls = document.getElementsByClassName('menu-controls')[0];
+
+    controls.classList.remove('menu-active');
+    navContainer.classList.remove('nav-active');
+});
