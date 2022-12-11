3c2efebba43fff7ce6928b461496f3ad90e1933a
Iceto04 <izetohr@gmail.com>
2022-12-11 19:44:13 +0200

    Create Diseases Controller and add routing for completed pages

---
+++ src/Web/iCareNow.Web/Controllers/DiseasesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iCareNow.Web.Controllers
+{
+    public class DiseasesController : BaseController
+    {
+        public DiseasesController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
