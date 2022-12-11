e7b773c16e1a8325cfcdd3a97279f26f4f3ebfbe
Iceto04 <izetohr@gmail.com>
2022-12-11 20:47:59 +0200

    Scaffold Identity pages

---
+++ src/Web/iCareNow.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(iCareNow.Web.Areas.Identity.IdentityHostingStartup))]
+namespace iCareNow.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
