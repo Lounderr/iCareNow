24243ebe29d871897f43c71cf7066334ff61f780
Lounderr <77444667+Lounderr@users.noreply.github.com>
2023-01-07 15:37:51 +0200

    Fix reset bug, redo HealthAtoZ routing, add About Us routing and add Forgotten password page

---
+++ src/Web/iCareNow.Web/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iCareNow.Web.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
