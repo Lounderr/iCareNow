646cf9ca3932864734a9d4ca935c1389d9921da4
Lounderr <77444667+Lounderr@users.noreply.github.com>
2023-01-09 21:39:41 +0200

    Add non-responsive Feedback page

---
+++ src/Web/iCareNow.Web/Controllers/FeedbackController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iCareNow.Web.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
