6b049787a8661a2245e69db0fa0da47cca4199d3
Hristo Slavchev <izetohr@gmail.com>
2023-02-01 14:07:18 +0200

    Introduce UserViewComponent to access the user data from everywhere

---
+++ src/Web/iCareNow.Web/ViewComponents/UserViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using iCareNow.Data.Models;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace iCareNow.Web.ViewComponents
+{
+    public class UserViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var user = this.userManager.GetUserAsync((ClaimsPrincipal)this.User).GetAwaiter().GetResult();
+
+            return this.Content(user.FirstName);
+        }
+    }
+}
