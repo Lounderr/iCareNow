5ae0fdbee0ba0d9031fe3e231cfe1c00099e0298
Hristo Slavchev <izetohr@gmail.com>
2023-02-03 13:01:30 +0200

    Remove old Mvc using

--- src/Web/iCareNow.Web.ViewModels/SearchArticleInputModel.cs
+++ src/Web/iCareNow.Web.ViewModels/SearchArticleInputModel.cs
@@ -1,9 +1,5 @@
 ﻿namespace iCareNow.Web.ViewModels
 {
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Web.Mvc;
-
     public class SearchArticleInputModel
     {
         public string Search { get; set; }
