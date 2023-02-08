122d9aa7f3da027d68841428961154840f0bfc3b
Lounderr <77444667+Lounderr@users.noreply.github.com>
2023-02-08 18:10:08 +0200

    Fix article TTS bugs

--- src/Web/iCareNow.Web/wwwroot/js/healthArticle.js
+++ src/Web/iCareNow.Web/wwwroot/js/healthArticle.js
@@ -1,33 +1,43 @@
-﻿const readArticleButton = document.querySelector(".tts-speaker-container");
+﻿window.onbeforeunload = (event) => {
+    synth.cancel();
+}
+
 const synth = window.speechSynthesis;
 
 function clickTtsBtn() {
     if (synth.speaking) {
         synth.cancel();
-
-        toggleButtonState();
     }
     else {
-        const articleTitle = document.querySelector(".article-title")?.textContent;
-        const articleDescription = document.querySelector(".article-description")?.textContent;
-        const articleContent = document.querySelector(".article-content")?.textContent;
+        const articleTitle = document.querySelector(".article-title").textContent;
+        const articleDescription = document.querySelector(".article-description").textContent;
+        const articleContent = document.querySelector(".article-content").textContent;
 
         const inputTxt = `${articleTitle}. ${articleDescription}. ${articleContent}`;
-        const utterThis = new SpeechSynthesisUtterance(inputTxt);
-        utterThis.lang = 'bg-BG';
-        synth.speak(utterThis);
 
-        utterThis.onstart = () => {
-            toggleButtonState()
-        }
 
-        utterThis.onend = () => {
-            toggleButtonState()
-        }
+        setTimeout(() => {
+            if (synth.getVoices().map(x => x.lang).includes("bg-BG")) {
+                const utterContent = new SpeechSynthesisUtterance(inputTxt);
+                utterContent.lang = "bg-BG";
+                synth.speak(utterContent);
+            }
+            else {
+                const noPackError = "Error. Bulgarian speech pack not installed!";
+                const utterError = new SpeechSynthesisUtterance(noPackError);
+                utterError.lang = "en-GB";
+                synth.speak(utterError);
+            }
+        }, 30)
+
     }
+
+    toggleButtonState();
 }
 
 function toggleButtonState() {
+    const readArticleButton = document.querySelector(".tts-speaker-container");
+
     for (let i = 0; i < readArticleButton.children.length; i++) {
         readArticleButton.children[i].classList.toggle("active");
     }
